c9f65191b4c7e515706f5b25a978a2e9be3f48bc
MrGultekin <gultekin.de@gmail.com>
2022-05-12 17:40:54 +0200

    Nav works

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import './App.scss'
 import {PageWelcome} from "./pages/PageWelcome";
 import {PageBooks} from "./pages/PageBooks";
 import {PageAbout} from "./pages/PageAbout";
+import {NavLink} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Personal Site</h1>
-        <hr/>
-        Welcome | Books | About
-        <hr/>
-        <PageWelcome/>
-        <PageBooks/>
-        <PageAbout/>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>Personal Site</h1>
+            <hr/>
+            <nav>
+                <NavLink to="/welcome">Welcome</NavLink> |
+                <NavLink to="/books"> Books </NavLink> |
+                <NavLink to="/about"> About </NavLink>
+            </nav>
+            <hr/>
+
+            <PageWelcome/>
+            <PageBooks/>
+            <PageAbout/>
+        </div>
+    )
 }
 
 export default App

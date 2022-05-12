77afab0079ee5f4ca2ad4a3377c058c9c6d4892f
MrGultekin <gultekin.de@gmail.com>
2022-05-12 16:25:38 +0200

    Basic navbar done.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
      <h1>Personal Site</h1>
+        <hr/>
+        Welcome | Books | About
+        <hr/>
         <PageWelcome/>
         <PageBooks/>
         <PageAbout/>

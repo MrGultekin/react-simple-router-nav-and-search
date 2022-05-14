3f75701f8ece0efa81c27efe372be56df0e32ffc
MrGultekin <gultekin.de@gmail.com>
2022-05-14 22:39:14 +0200

    There are 30 books yet.

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,8 +1,19 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const booksURL='https://gutendex.com/books/?search=berlin';
 export const PageBooks = () => {
+    const [bookData,setBookData] = useState({count:0});
+    useEffect(() =>{
+        (async () =>{
+            setBookData((await axios.get(booksURL)).data);
+        })()
+    },[]);
+
     return (
         <>
             <h1>Books</h1>
-            <p>There are no books yet.</p>
+            <p>There are {bookData.count} books yet.</p>
         </>
     )
 }

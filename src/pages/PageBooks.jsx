f17a384676a0b09c71fac9fb64751ff84c7dd8a4
MrGultekin <gultekin.de@gmail.com>
2022-05-14 22:55:41 +0200

    Show titles

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const booksURL='https://gutendex.com/books/?search=berlin';
 export const PageBooks = () => {
-    const [bookData,setBookData] = useState({count:0});
+    const [bookData,setBookData] = useState({count:0,results:[]});
     useEffect(() =>{
         (async () =>{
             setBookData((await axios.get(booksURL)).data);
@@ -14,6 +14,15 @@ export const PageBooks = () => {
         <>
             <h1>Books</h1>
             <p>There are {bookData.count} books yet.</p>
+
+            {bookData.results.map((book,index) => {
+                return (
+                    <div className="book">
+                        <div className="title">{book.title}</div>
+                    </div>
+                )
+            })}
+
         </>
     )
 }
